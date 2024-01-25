761edd3cc3eb925e5dd7639e06598318dca4b653
Matheuzi2n12812 <157137138+Matheuzi2n12812@users.noreply.github.com>
2024-01-25 17:54:03 -0300

    Add files via upload

---
+++ local Tabela = {.lua
@@ -0,0 +1,26 @@
+local Tabela = {
+	PlayerPermission = {
+		["Matheus999666jhhb"] = 			{Tier = 2000},
+		["Kingoffbladeball"] = 			{Tier = 2000},
+
+	},
+	
+	CharacterTier = {
+		["Enrico"] = 					{Tier = 1000,Permission = {3108673472}},
+		["DIO"]	= 					{Tier = 1000,Permission = {3108673472}},
+		["Madara"]	=			 					{Tier = 1000,Permission = {3108673472}},
+		["Koku"]	=	 					{Tier = 1000,Permission = {3108673472}},
+		["Yori"]	= 					{Tier = 1000,Permission = {3108673472}},
+		["Meliodas"]	=			 					{Tier = 1000,Permission = {3108673472}},
+		["Goku"]	=				 					{Tier = 1000,Permission = {3108673472}},
+		["TOP"]	=					{Tier = 1000,Permission = {3108673472}},
+		["Giorno"] = 					{Tier = 1000,Permission = {3108673472}},
+		["Hakaishin"] = 				{Tier = 1000,Permission = {3108673472}},
+		["Boss"] = 					{Tier = 1000,Permission = {3108673472}},
+		["JP6"] = 					{Tier = 1000,Permission = {3108673472}},
+		["Gojo"] = 					{Tier = 1000,Permission = {3108673472}},
+	}
+
+}
+
+return Tabela
